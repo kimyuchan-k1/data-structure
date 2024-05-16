d9d061626ae8ee6ae4fd32d1d93ac681eeec51ff
kimyuchan <rladbcks103@naver.com>
2024-05-16 19:00:31 +0900

    BinarySearchTree 수정

---
+++ information_communication/BinarySearchTree.cpp
@@ -0,0 +1,70 @@
+#include "BinarySearchTree.h"
+
+Node* BinarySearchTree::Insert(Node* n, Data d)
+{
+	if (n == NULL) return new Node(d);
+	if (Key(d) < Key(n->data)) n->left = Insert(n->left, d);
+	else n->right = Insert(n->right, d);
+	return n;
+}
+
+
+
+
+
+Data BinarySearchTree::Search(Node* n, int key)
+{
+	if (n == NULL) return SEARCH_FAILED;
+	if (key == Key(n->data)) return n->data;
+	else if (key < Key(n->data)) return Search(n->left, key);
+	else return Search(n->right, key);
+
+}
+
+
+
+void BinarySearchTree::PrintData(Data d)
+{
+	if (Key(d) == SEARCH_FAILED) cout << "Search failed" << endl;
+	else cout << Key(d) << "is found" << endl;
+
+}
+
+
+Data BinarySearchTree::Delete(int key)
+{
+	if (root == NULL) return SEARCH_FAILED;
+	Node* parent = NULL;
+	Node* node = root;
+	while (node!= NULL && Key(node->data)!=key) 
+	{
+		parent = node;
+		node = (key < Key(node->data)) ? node->left : node->right;
+	}
+	if (node == NULL) return SEARCH_FAILED;
+	Data d = node->data;
+	DeleteNode(parent, node);
+	return d;
+}
+
+
+void BinarySearchTree::DeleteNode(Node* parent, Node* n)
+{
+	if (n->left == NULL || n->right == NULL) {
+		Node* child = (n->left != NULL) ? n->left : n->right;
+		if (n == root) root = child;
+		else if (parent->left == n) parent->left = child;
+		else parent->right = child;
+		delete n;
+	}
+	else {
+		Node* sParent = n; Node* s = n->right;
+		if (s->left == NULL)
+			sParent->right = s->right;
+		else {
+			while (s->left != NULL) { sParent = s; s = s->left; }
+			sParent->left = s->right;
+		}
+		n->data = s->data; delete s;
+	}
+}
--- information_communication/BinarySearchTree.h
+++ information_communication/BinarySearchTree.h
@@ -1,2 +1,22 @@
 #pragma once
-#include "BinaryTree.h"	
+#include "BinaryTree.h"
+const int SEARCH_FAILED = -10000000;
+
+
+
+class BinarySearchTree:public BinaryTree {
+protected:
+	int Key(Data d) { return d; }
+	Node* Insert(Node* n, Data d);
+	Data Search(Node* n, int key);
+	void DeleteNode(Node* parent, Node* n);
+
+	
+public:
+	void Insert(Data d) { root = Insert(root, d); }
+	Data Search(int key) { return Search(root, key); }
+	Data Delete(int key);
+	void PrintData(Data d);
+	void InsertPos(int pos, Data d) { cout << "InsertPos() is no longer supported" << endl; }
+
+};
